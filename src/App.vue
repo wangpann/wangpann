0ff872c0a9cdef5a18ccfdeac575d84540111e12
wangpann <1063668275@qq.com>
2021-04-20 17:05:30 +0800

    .

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite111" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
 <script lang="ts">
